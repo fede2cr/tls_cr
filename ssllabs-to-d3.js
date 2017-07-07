fea9c74b622e5dfaafadcaade8b58e6fe2ba92f6
Federico Figueroa <ffc.figueroa@gmail.com>
2017-07-07 16:19:45 -0600

    Parse SSLLabs data and create a data model for D3 to use

--- ssllabs-to-d3.js
+++ ssllabs-to-d3.js
@@ -1,24 +1,49 @@
-var ssllabs = require("./ssllabs.json");
+var ssllabs = require("./data/ssllabs.json");
 var ssltree = {
-    "name": "Costa Rica",
-    "children": [
-      { 
-        "name": "Go.cr",
-        "children": [
-          { "name": "Son of A" },
-          { "name": "Daughter of A" }
-        ]
-      },
-      { "name": "Fi.cr" }
-    ]
+  "name": "Costa Rica",
+  "children": [
+    {
+      "name": "go.cr",
+      "children": []
+    },
+    {
+      "name": "fi.cr",
+      "children": []
+    }
+  ]
 };
 
-	
-//console.log(ssllabs[0].host);
-
-for(var hostKey in ssllabs) {
-    console.log("key:"+hostKey+", value:"+ssllabs[hostKey].host);
-    ssltree[hostKey] = (ssllabs[hostKey].host);
+let getKey = (hostname) => {
+  /**
+   * Aquí la idea es sacar del hostname el identificador
+   * que ayude a encontrar en ssltree la rama correcta
+   */
+  hostname = hostname.replace(/\/\s*$/, ''); //Si hay un slash al final, quite quite
+  hostname = hostname.split('.'); //Parta el string en un array usando el punto como separador
+  return `${hostname[hostname.length-2]}.${hostname[hostname.length-1]}`; // un string compuesto de los dos últimos campos del array
 }
 
-console.log(ssltree);
+/**
+ * Iteramos por los ítems del array que viene en el archivo json
+ */
+ssllabs.forEach(item => {
+
+  let key = getKey(item.host);
+  let index = ssltree.children.findIndex(i => i.name === key); // Qué rama es
+
+  if(index !== -1) {
+    ssltree.children[index].children.push({
+      name: item.host,
+      grade: item.endpoints[0].grade ? item.endpoints[0].grade : 'F', // Operador ternario por si no hay grade, ponga "F"
+      protocols: item.endpoints[0].details.protocols,
+      supportsRc4: item.endpoints[0].details.supportsRc4,
+      forwardSecrecy: item.endpoints[0].details.forwardSecrecy
+    })
+  }
+});
+
+/**
+ * Aquí en vez de a consola hay que tirarlo a un archivo, pero por ahora y para
+ * facilidad de desarrollo, va pa'consola
+ */
+console.log(JSON.stringify(ssltree, null, '  '));
